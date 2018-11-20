879633f8e3aa257aa832d7af14cd1dc016ec3c18
Lennert Verwimp <lennert.verwimp@student.ucll.be>
2018-11-20 13:28:04 +0100

    dbmax script working

--- scripts/dbmax.php
+++ scripts/dbmax.php
@@ -1,10 +1,10 @@
 <?php
-$connection=mysqli_connect("localhost","root","","check");
+$connection=mysqli_connect("localhost","kanalles","","check");
 if($connection){
 } else {
         die("Connection failed. Reason: ".mysqli_connect_error());
 }
-$sql="DELETE FROM check.log WHERE id not in (SELECT id FROM (SELECT check.log ORDER BY id DESC LIMIT 43))"
+$sql="DELETE FROM check.log WHERE id not in (SELECT id FROM (SELECT id FROM check.log ORDER BY id DESC LIMIT 44) foo)";
 mysqli_query($connection,$sql);
 mysqli_close($connection);
 ?>
