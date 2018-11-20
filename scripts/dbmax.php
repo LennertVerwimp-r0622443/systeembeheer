c1190f08d4041a66c010d0651bc46ddb5eae152b
Lennert Verwimp <lennert.verwimp@student.ucll.be>
2018-11-20 13:11:25 +0100

    script changed

--- scripts/dbmax.php
+++ scripts/dbmax.php
@@ -4,7 +4,7 @@ if($connection){
 } else {
         die("Connection failed. Reason: ".mysqli_connect_error());
 }
-$sql="DELETE FROM check.log WHERE id not in (SELECT id FROM (SELECT check.log ORDER BY id DESC LIMIT 43) foo)"
+$sql="DELETE FROM check.log WHERE id not in (SELECT id FROM (SELECT check.log ORDER BY id DESC LIMIT 43))"
 mysqli_query($connection,$sql);
 mysqli_close($connection);
 ?>
