5cac152e4c78e560dc3a158f63e9d1e9ad69866e
Lennert Verwimp <lennert.verwimp@student.ucll.be>
2018-11-20 13:30:12 +0100

    nog prutsen met script

--- scripts/dbmax.php
+++ scripts/dbmax.php
@@ -4,7 +4,7 @@ if($connection){
 } else {
         die("Connection failed. Reason: ".mysqli_connect_error());
 }
-$sql="DELETE FROM check.log WHERE id not in (SELECT id FROM (SELECT id FROM check.log ORDER BY id DESC LIMIT 44) foo)";
+$sql="DELETE FROM check.log WHERE id not in (SELECT id FROM (SELECT id FROM check.log ORDER BY id DESC LIMIT 85))";
 mysqli_query($connection,$sql);
 mysqli_close($connection);
 ?>
