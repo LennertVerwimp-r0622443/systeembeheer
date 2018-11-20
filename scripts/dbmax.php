14247011c1e7d67d8c904979ae137e7a8062ca64
Lennert Verwimp <lennert.verwimp@student.ucll.be>
2018-11-20 13:02:39 +0100

    added query to db script

--- scripts/dbmax.php
+++ scripts/dbmax.php
@@ -1,5 +1,10 @@
 <?php
 $connection=mysqli_connect("localhost","root","","check");
-
+if($connection){
+} else {
+        die("Connection failed. Reason: ".mysqli_connect_error());
+}
+$sql="DELETE FROM check.log WHERE id not in (SELECT id FROM check.log ORDER BY id DESC LIMIT 43) foo )"); 
+mysqli_query($connection,$sql);
 mysqli_close($connection);
 ?>
